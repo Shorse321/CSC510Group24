60d107df8bef9a52964b1a1e214beaf27029bcc1
Sailesh0910 <sailesh.s.0910@gmail.com>
2025-10-20 14:14:27 -0400

    added gitignore and .env modifications

--- proj2/stackshack/create_admin.py
+++ proj2/stackshack/create_admin.py
@@ -4,9 +4,10 @@ from models.user import User
 
 app = create_app()
 with app.app_context():
-    # Create admin user (1st time only)
+    # Create admin user
     admin = User(username='admin', role='admin')
-    admin.set_password('admin')  # Use a secure password
+    # IMPORTANT: Use a secure, temporary password. Change it immediately after first login.
+    admin.set_password('admin') 
     db.session.add(admin)
     db.session.commit()
     print("Admin user created")
