e3c517ed7c436e16511e12cea1274957a1384500
Swetha Manivasagam <98175379+SwethaatGH@users.noreply.github.com>
2025-12-09 13:49:01 -0500

    Update jest.bulk.config.mjs

--- proj3/SE_G25-main/proj2/backend/jest.bulk.config.mjs
+++ proj3/SE_G25-main/proj2/backend/jest.bulk.config.mjs
@@ -3,7 +3,8 @@ export default {
   transform: {},
   moduleFileExtensions: ['js', 'json', 'mjs'],
   testMatch: ['**/__tests__/**/bulkFeature.test.js', '**/__tests__/**/orderController.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
+  // Remove or comment out this line:
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
   collectCoverageFrom: [
     'controllers/**/*.js',
     'models/**/*.js',
